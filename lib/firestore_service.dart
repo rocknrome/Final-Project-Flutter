5e7ee29ad005e9cbce2b5d76865916e75a6b0dae
Roman Larionov <pcsaveddotcom@gmail.com>
2024-04-07 14:04:36 -0400

    receiving data from Firestore DB

--- lib/firestore_service.dart
+++ lib/firestore_service.dart
@@ -3,13 +3,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<void> addDocument() async {
-    await _firestore.collection('your_collection_name').add({
-      'field1': 'value1',
-      'field2': 'value2',
-      // Add other fields as needed
+  Future<void> addCar() async {
+    await _firestore.collection('cars').add({
+      'id': 'D7w44AwVohB4LnVTSnLc',
+      'make': 'Toyota',
+      'model': 'Corolla',
+      'color': 'Blue',
+      'year': 2021,
+      'mileage': 18000,
+      'price': 24000,
+      'description': 'Reliable everyday commuter',
+      'photo_url':
+          'https://live.staticflickr.com/65535/53632512847_071816a4c2.jpg',
     });
   }
 
-  // You can add more methods here for reading, updating, and deleting documents
+  Future<List<DocumentSnapshot<Map<String, dynamic>>>> fetchCars() async {
+    var querySnapshot = await _firestore.collection('cars').get();
+    return querySnapshot.docs;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import './firestore_service.dart';
 
 void main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // Ensure plugin services are initialized
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-        apiKey: "AIzaSyAHnujk7gFv0ln_bHKWwO5FksCL_xqDV7A", // Use your apiKey
-        authDomain: "rnr-car-club.firebaseapp.com", // Use your authDomain
-        projectId: "rnr-car-club", // Use your projectId
-        storageBucket: "rnr-car-club.appspot.com", // Use your storageBucket
-        messagingSenderId: "766735246500", // Use your messagingSenderId
-        appId: "1:766735246500:web:5a7ba925dee0b51e3df231", // Use your appId
-        measurementId: "G-RDMV30HF5W" // Optional and used only for analytics
-        ),
+      apiKey: "AIzaSyAHnujk7gFv0ln_bHKWwO5FksCL_xqDV7A",
+      authDomain: "rnr-car-club.firebaseapp.com",
+      projectId: "rnr-car-club",
+      storageBucket: "rnr-car-club.appspot.com",
+      messagingSenderId: "766735246500",
+      appId: "1:766735246500:web:5a7ba925dee0b51e3df231",
+      measurementId: "G-RDMV30HF5W",
+    ),
   );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key}); // Added a Key parameter
+  MyApp({super.key}); // Added named key parameter
 
   final FirestoreService _firestoreService = FirestoreService();
 
@@ -31,13 +31,48 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Firestore Example'),
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              _firestoreService.addDocument();
-            },
-            child: const Text('Add Document'),
-          ),
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                _firestoreService.addCar();
+              },
+              child: const Text('Add Car'),
+            ),
+            Expanded(
+              child:
+                  FutureBuilder<List<DocumentSnapshot<Map<String, dynamic>>>>(
+                future: _firestoreService.fetchCars(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (snapshot.hasData) {
+                    return ListView(
+                      children: snapshot.data!.map((doc) {
+                        final data = doc.data();
+                        if (data != null) {
+                          return ListTile(
+                            title: Text(
+                                "${data['make'] ?? ''} ${data['model'] ?? ''}"),
+                            subtitle: Text(
+                                'Year: ${data['year'] ?? ''} - Price: \$${data['price'] ?? ''}'),
+                            leading: Image.network(data['photo_url'] ?? '',
+                                width: 100, fit: BoxFit.cover),
+                          );
+                        } else {
+                          return const SizedBox(); // Or handle null data in another way
+                        }
+                      }).toList(),
+                    );
+                  } else {
+                    return const Text('No data');
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
